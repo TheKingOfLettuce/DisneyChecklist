972187920b6c60ee941a9e196d54e72494d2f3d2
TheKingOfLettuce <68159617+TheKingOfLettuce@users.noreply.github.com>
2025-04-16 22:44:52 -0400

    update PWA

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,12 +1,30 @@
+const cacheName = 'disney-checklist-cache-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './Build/DisneyChecklist.loader.js',
+  './Build/DisneyChecklist.framework.js',
+  './Build/DisneyChecklist.wasm',
+  './Build/DisneyChecklist.data',
+  './img/D180.png',
+  './img/512.png',
+];
+
 self.addEventListener("install", function (e) {
-    console.log("Service worker installed.");
-    self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", function (e) {
-    console.log("Service worker activated.");
-  });
-  
-  self.addEventListener("fetch", function (e) {
-    
-  });
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+  console.log("Service worker installed.");
+});
+
+self.addEventListener("activate", function (e) {
+  console.log("Service worker activated.");
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(response =>
+      response || fetch(e.request)
+    )
+  );
+});
